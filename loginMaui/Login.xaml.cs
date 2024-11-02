1ea3d85b5a7b4a2ab7c549660d475d2ad6bd989e
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-02 15:31:26 -0300

    fazendo verufucaçao de login

--- loginMaui/Login.xaml.cs
+++ loginMaui/Login.xaml.cs
@@ -7,15 +7,47 @@ public partial class Login : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
 		try { 
-		
-		
+		 List <DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+			{
+				new DadosUsuario()
+				{
+					Usuario = "Jose",
+					Senha = "123"
+				},
+                new DadosUsuario()
+                {
+                    Usuario = "Maria",
+                    Senha = "123"
+                }
+            };//fecha list
+
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+			//linq
+			if(lista_usuarios.Any(
+				i => (dados_digitados.Usuario == i.Usuario && 
+			      dados_digitados.Senha == i.Senha)) )
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+			 App.Current.MainPage = new Protegida();
+			
+			} else
+			{
+				throw new Exception("Usuario ou senha incorretos");
+
+
+			}
+
+
 		
 		}catch (Exception ex) {
-			DisplayAlert("Ops", ex.Message, "Fechar");
-		
+			await DisplayAlert("Ops", ex.Message, "Fechar");
 		
 		
 		}//fecha o cath
