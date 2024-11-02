afef541d6ae2b97b1f38c98ba345577a420a154b
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-02 14:54:18 -0300

    Adicionar arquivos de projeto.

---
+++ loginMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace loginMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ loginMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace loginMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
