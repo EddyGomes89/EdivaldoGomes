5e2562bc02c459cb9ce7f288dbfc24b91e8a3df1
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-02 15:08:44 -0300

    interface login

--- loginMaui/App.xaml.cs
+++ loginMaui/App.xaml.cs
@@ -7,7 +7,7 @@ namespace loginMaui
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Login();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
