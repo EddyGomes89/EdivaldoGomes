ecf679fbd49fcaa52306948af2af72459ea6cc22
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-02 15:14:06 -0300

    criando classe login para usuario e senha

---
+++ loginMaui/DadosUsuario.cs
@@ -0,0 +1,11 @@
+﻿
+namespace loginMaui
+{
+    public class DadosUsuario
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+
+
+    }
+}
--- loginMaui/Login.xaml.cs
+++ loginMaui/Login.xaml.cs
@@ -9,6 +9,15 @@ public partial class Login : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-
-    }
-}
+		try { 
+		
+		
+		
+		}catch (Exception ex) {
+			DisplayAlert("Ops", ex.Message, "Fechar");
+		
+		
+		
+		}//fecha o cath
+    }//fecha button
+}//fecha classe
